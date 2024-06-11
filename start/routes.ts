b9e8b10030592bee73d76e4e7f3dc71675d6d34c
Crolweb <crolweb@gmail.com>
2024-06-11 18:38:15 +0200

    [MERGE] route from samuel

--- start/routes.ts
+++ start/routes.ts
@@ -14,20 +14,24 @@ const RegistersController = () => import('#controllers/auth/registers_controller
 // import RegistersController from '#controllers/registers_controller'
 import { middleware } from '#start/kernel'
 
-
-router.get('/', async (ctx) => {
+router
+  .get('/', async (ctx) => {
     await ctx.auth.check()
     return ctx.view.render('pages/home')
-}).as('home')
+  })
+  .as('home')
 
 router.on('/test').render('pages/test').as('test').use(middleware.auth())
 
-router.group(() => {
+router
+  .group(() => {
     router.get('/login', [LoginController, 'show']).as('login.show')
     router.post('/login', [LoginController, 'store']).as('login.store')
-    
+
     router.post('/logout', [LogoutController, 'handle']).as('logout')
-}).as('auth').use(middleware.auth())
+  })
+  .as('auth')
+  .use(middleware.auth())
 router.get('/register', [RegistersController, 'index']).as('register.index')
 router.post('/register', [RegistersController, 'store']).as('register.store')
 // router.resource('/register', RegistersController)

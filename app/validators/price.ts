d3a700948ff8f7b57a89a39db6d50aae2f63c572
samuel-villa <ciullasamuel@gmail.com>
2024-06-17 14:08:50 +0200

    [IMP] description => price_description

--- app/validators/price.ts
+++ app/validators/price.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 
 export const createPriceValidator = vine.compile(
     vine.object({
-      description: vine.string().maxLength(255),
+      price_description: vine.string().maxLength(255),
       regular_price: vine.number().positive(),
       discounted_price: vine.number().positive(),
       available_qty: vine.number().positive(),

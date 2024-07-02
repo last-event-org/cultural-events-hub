4a13bd55a2dce06248745ee0c423f8f89fa4bf01
Crolweb <crolweb@gmail.com>
2024-07-02 13:20:15 +0200

    [IMP] update input in the search form and auto-focus

--- resources/js/filters-buttons/dropdown_cities.js
+++ resources/js/filters-buttons/dropdown_cities.js
@@ -1,18 +1,41 @@
-  //drop-down city list
-  const section = document.getElementById('big-search-bar-container');
-  document.addEventListener('DOMContentLoaded', function () {
-    const buttonCity = document.getElementById('button-city');
-    const cityList = document.getElementById('city-list');
+//drop-down city list
+const section = document.getElementById('big-search-bar-container')
+const cityInput = document.getElementById('city-chosen')
+const buttonCity = document.getElementById('button-city')
+const cityList = document.getElementById('city-list')
+const mapPopup = document.getElementById('chosen-radius')
 
-    buttonCity.addEventListener('click', function () {
-      cityList.classList.remove('hidden');
+cityInput.addEventListener('focus', (e) => {
+  cityList.classList.remove('hidden')
+})
 
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  eventListenerOnList()
 
-    document.addEventListener('click', function (event) {
-      const isClickInside = buttonCity.contains(event.target) || cityList.contains(event.target);
-      if (!isClickInside) {
-        cityList.classList.add('hidden');
-      }
-    });
-  });
+  buttonCity.addEventListener('click', () => {
+    cityList.classList.contains('hidden')
+      ? cityList.classList.remove('hidden')
+      : cityList.classList.add('hidden')
+  })
+
+  document.addEventListener('click', (event) => {
+    const isClickInside = buttonCity.contains(event.target) || cityList.contains(event.target)
+    if (!isClickInside) {
+      cityList.classList.add('hidden')
+    }
+  })
+})
+
+function updateCity(city) {
+  cityInput.value = city
+  mapPopup.focus()
+}
+
+function eventListenerOnList() {
+  const cities = document.getElementsByName('city')
+  cities.forEach((city) => {
+    city.addEventListener('click', (e) => {
+      updateCity(e.target.value)
+    })
+  })
+}

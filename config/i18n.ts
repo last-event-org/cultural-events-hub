6d75a09d390f7b75e9e0e0c9b38bf6fbf138658d
samuel-villa <ciullasamuel@gmail.com>
2024-07-02 09:11:42 +0200

    [ADD] started setup internationalization

---
+++ config/i18n.ts
@@ -0,0 +1,29 @@
+import app from '@adonisjs/core/services/app'
+import { defineConfig, formatters, loaders } from '@adonisjs/i18n'
+
+const i18nConfig = defineConfig({
+  defaultLocale: 'en',
+  formatter: formatters.icu(),
+
+  loaders: [
+    /**
+     * The fs loader will read translations from the
+     * "resources/lang" directory.
+     *
+     * Each subdirectory represents a locale. For example:
+     *   - "resources/lang/en"
+     *   - "resources/lang/fr"
+     *   - "resources/lang/it"
+     */
+    loaders.fs({
+      location: app.languageFilesPath()
+    }),
+  ],
+  fallbackLocales: {
+    'fr-FR': 'fr',
+    'en': 'en'
+  },
+  supportedLocales: ['fr', 'en']
+})
+
+export default i18nConfig

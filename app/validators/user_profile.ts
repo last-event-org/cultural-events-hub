5c8a4ba0d1d40dbd818b712d8e5bd94fc20f2c84
Crolweb <crolweb@gmail.com>
2024-07-05 15:17:07 +0200

    [FIX] fix save update profile when change is done

--- app/validators/user_profile.ts
+++ app/validators/user_profile.ts
@@ -1,28 +1,11 @@
 import vine from '@vinejs/vine'
 
-
 export const updateUserProfileMandatoryValidator = vine.compile(
-    vine.object({
-        first_name: vine
-            .string()
-            .escape()
-            .trim()
-            .alpha()
-            .maxLength(255),
-        last_name: vine
-            .string()
-            .escape()
-            .trim()
-            .alpha()
-            .maxLength(255),
-        email: vine
-            .string()
-            .trim()
-            .escape()
-            .email()
-            .maxLength(255)
-            .normalizeEmail({
-                all_lowercase: true,
-            }),
-        })
+  vine.object({
+    first_name: vine.string().escape().maxLength(255),
+    last_name: vine.string().escape().maxLength(255),
+    email: vine.string().trim().escape().email().maxLength(255).normalizeEmail({
+      all_lowercase: true,
+    }),
+  })
 )
--- app/validators/vendor_data.ts
+++ app/validators/vendor_data.ts
@@ -1,22 +1,27 @@
 import vine from '@vinejs/vine'
 
-
 export const createVendorDataValidator = vine.compile(
-    vine.object({
-        company_name: vine
-            .string()
-            // .escape()
-            .maxLength(255)
-            .optional(),
-        vat_number: vine
-            .string()
-            .escape()
-            .trim()
-            .maxLength(12)
-            // regex rules: 
-            // - must start with 'BE'
-            // - 10 numbers from index 2 to 13
-            // - fix numbers of characters (12)
-            .regex(/^BE\d{10}$/),
-    })
+  vine.object({
+    company_name: vine
+      .string()
+      // .escape()
+      .maxLength(255)
+      .optional(),
+    vat_number: vine
+      .string()
+      .escape()
+      .trim()
+      .maxLength(12)
+      // regex rules:
+      // - must start with 'BE'
+      // - 10 numbers from index 2 to 13
+      // - fix numbers of characters (12)
+      .regex(/^BE\d{10}$/)
+      .optional(),
+    street: vine.string().escape().maxLength(255).optional().requiredIfExists('vat_number'),
+    number: vine.string().escape().maxLength(10).optional().requiredIfExists('vat_number'),
+    zip_code: vine.number().positive().withoutDecimals().optional().requiredIfExists('vat_number'),
+    city: vine.string().escape().maxLength(255).optional().requiredIfExists('vat_number'),
+    country: vine.string().escape().maxLength(255).optional().requiredIfExists('vat_number'),
+  })
 )

30c31d824f79f935902770cc439c7e900da3c3f0
Crolweb <crolweb@gmail.com>
2024-07-09 00:27:30 +0200

    [FIX] fix problems with the map and merge home with eventslist

--- resources/js/map_home.js
+++ resources/js/map_home.js
@@ -1,44 +1,28 @@
+import { getCoordinatesFromCity } from './geolocation'
+
 const sliderInput = document.getElementById('slider-input')
 const sliderThumb = document.getElementById('slider-thumb')
 const progressBar = document.getElementById('progress-bar')
 const inputRadius = document.getElementById('chosen-radius')
 const sliderValue = document.getElementById('slider-value').children[0]
+const cityInput = document.getElementById('city-chosen')
+const cityList = document.getElementById('city-list')
+const buttonCity = document.getElementById('button-city')
 
 // Bruxelles
 // let [latitude, longitude] = [50.85045, 4.34878]
+// Liège
 let [latitude, longitude] = [50.645138, 5.57342]
-// let latitude = window.latitude === undefined ? window.latitude : 50.645138
-// let longitude = window.longitude ? 2 : 5.57342
 
-let mapZoom = 13
+let mapZoom = 10
 let circle
 let poi = []
 let events
-let map = L.map('map', {
-  scrollWheelZoom: false, // Désactive le zoom par molette par défaut
-})
-
-// console.log('MAP HOME LAT LONG')
-// console.log(latitude + '    ' + longitude)
-// console.log(window.latitude + '    ' + window.longitude)
+let map
 
 sliderInput.addEventListener('input', updateSlider)
 
-setTimeout(function () {
-  map.invalidateSize() // Redimensionne la carte après un léger délai
-}, 500)
-
-// poi.forEach(function (point) {
-//   L.marker([point.lat, point.lng]).addTo(map).bindPopup(point.name)
-// })
-
 let baseRadius = sliderInput.value * 1000
-// let circle = L.circle([latitude, longitude], {
-//   color: 'rgb(234 179 8)',
-//   fillColor: 'rgb(234 179 8)',
-//   fillOpacity: 0.2,
-//   radius: baseRadius,
-// }).addTo(map)
 
 sliderInput.addEventListener('change', () => {
   inputRadius.value = sliderInput.value
@@ -61,6 +45,136 @@ function updateSlider() {
 function updateCircleRadius(newRadius) {
   circle.setRadius(newRadius)
 }
+
+async function createMap(lat = latitude, long = longitude, update = false) {
+  if (update === false) {
+    if (window.latitude) lat = window.latitude
+    if (window.longitude) long = window.longitude
+  }
+  if (map === undefined) {
+    map = L.map('map', {
+      scrollWheelZoom: false, // Désactive le zoom par molette par défaut
+    })
+  }
+  map.setView([lat, long], mapZoom)
+  setTimeout(function () {
+    map.invalidateSize() // Redimensionne la carte après un léger délai
+  }, 500)
+
+  // Ajouter les tuiles OSM
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map)
+
+  if (circle !== undefined) {
+    circle.remove()
+  }
+
+  circle = L.circle([lat, long], {
+    color: 'rgb(234 179 8)',
+    fillColor: 'rgb(234 179 8)',
+    fillOpacity: 0.2,
+    radius: baseRadius,
+    // Rayon en mètres, variable à changer pour augmenter
+    // le taille du cercle mais il faut aussi changer l'appel
+    // de la fonction displayPOIsWithinRadius
+    // pour récupérer les POI
+  }).addTo(map)
+  await getEventsSearch(urlParams)
+  createPois(events)
+}
+
+function createPois(eventsPoi) {
+  console.log('createPois')
+  eventsPoi.forEach((event) => {
+    // poi.push({
+    //   id: event.id,
+    //   title: event.title,
+    //   lat: event.location.latitude,
+    //   lng: event.location.longitude,
+    //   location: event.location.name,
+    //   category: event.categoryTypes[0].category.slug,
+    // })
+    let popup = L.popup().setContent(
+      `<a href="/events/${event?.id ?? ''}"'>${event.title}</a><br/>${event.location.name}`
+    )
+    let myIcon = L.icon({
+      className: `bg-${event.categoryTypes[0].category.slug} rounded-full m-2`,
+      iconSize: [24, 24],
+      iconUrl: `/svg/categories/${event.categoryTypes[0].category.slug}.svg`,
+    })
+    L.marker([event.location.latitude, event.location.longitude], { icon: myIcon })
+      .addTo(map)
+      .bindPopup(popup)
+  })
+}
+
+createMap(latitude, longitude)
+
+document.addEventListener('DOMContentLoaded', () => {
+  eventListenerOnList()
+  const urlParams = new URLSearchParams(window.location.search)
+  if (urlParams.get('city') !== '') {
+    cityInput.value = urlParams.get('city')
+  }
+
+  buttonCity.addEventListener('click', () => {
+    cityList.classList.contains('hidden')
+      ? cityList.classList.remove('hidden')
+      : cityList.classList.add('hidden')
+  })
+
+  document.addEventListener('click', (event) => {
+    const isClickInside = buttonCity.contains(event.target) || cityList.contains(event.target)
+    if (!isClickInside) {
+      cityList.classList.add('hidden')
+    }
+  })
+})
+
+async function updateCity(city) {
+  cityInput.value = city
+  ;[latitude, longitude] = await getCoordinatesFromCity(city)
+  map.setView([latitude, longitude], mapZoom)
+  await createMap(latitude, longitude, true)
+}
+
+function eventListenerOnList() {
+  const cities = document.getElementsByName('city-list')
+  cities.forEach((city) => {
+    city.addEventListener('click', (e) => {
+      updateCity(e.target.value)
+    })
+  })
+}
+
+async function getEventsSearch(params) {
+  try {
+    const response = await fetch(`/api/search?${params}`)
+    events = await response.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function updateMapZoom(radiusKm) {
+  let zoomLevel
+  if (radiusKm <= 1) {
+    zoomLevel = 13
+  } else if (radiusKm <= 5) {
+    zoomLevel = 12
+  } else if (radiusKm <= 10) {
+    zoomLevel = 11
+  } else if (radiusKm <= 17) {
+    zoomLevel = 10
+  } else {
+    zoomLevel = 9
+  }
+}
+
+// Initialize the slider position and value
+updateSlider()
 
 // Fonction pour calculer la distance entre deux points (en km)
 // function getDistance(lat1, lon1, lat2, lon2) {
@@ -95,124 +209,3 @@ function updateCircleRadius(newRadius) {
 
 //   poiList.appendChild(ul)
 // }
-
-export async function createMap() {
-  // console.log('CREATE MAP')
-  // console.log(lat + '   ' + long)
-  if (window.latitude) latitude = window.latitude
-  if (window.longitude) longitude = window.longitude
-  // console.log(lat + '   ' + long)
-  map.setView([latitude, longitude], mapZoom)
-
-  // Ajouter les tuiles OSM
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map)
-  // L.removelayer(circle)
-  // L.circle().removeFrom(map)
-  circle = L.circle([latitude, longitude], {
-    color: 'rgb(234 179 8)',
-    fillColor: 'rgb(234 179 8)',
-    fillOpacity: 0.2,
-    radius: baseRadius,
-    // Rayon en mètres, variable à changer pour augmenter
-    // le taille du cercle mais il faut aussi changer l'appel
-    // de la fonction displayPOIsWithinRadius
-    // pour récupérer les POI
-  }).addTo(map)
-  const urlParams = new URLSearchParams(window.location.search)
-  console.log(urlParams)
-  // console.log('api/search?' + urlParams)
-  // for (const [key, value] of urlParams) {
-  //   console.log(key + '  ' + value)
-  // }
-  await getEventsSearch(urlParams)
-  // await getAllEvents()
-  // await getEvents()
-  createPois(events)
-}
-
-function createPois(eventsPoi) {
-  console.log('createPois')
-  eventsPoi.forEach((event) => {
-    // poi.push({
-    //   id: event.id,
-    //   title: event.title,
-    //   lat: event.location.latitude,
-    //   lng: event.location.longitude,
-    //   location: event.location.name,
-    //   category: event.categoryTypes[0].category.slug,
-    // })
-    let popup = L.popup().setContent(
-      `<a href="/events/${event?.id ?? ''}"'>${event.title}</a><br/>${event.location.name}`
-    )
-    let myIcon = L.icon({
-      className: `bg-${event.categoryTypes[0].category.slug} rounded-full m-2`,
-      iconSize: [24, 24],
-      iconUrl: `/svg/categories/${event.categoryTypes[0].category.slug}.svg`,
-    })
-    L.marker([event.location.latitude, event.location.longitude], { icon: myIcon })
-      .addTo(map)
-      .bindPopup(popup)
-  })
-}
-
-async function getEvents() {
-  try {
-    const response = await fetch('/api/getEvents')
-    events = await response.json()
-    // console.log(events)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function getEventsSearch(params) {
-  try {
-    // console.log('\n\ngetEventsSearch\n\n')
-    // console.log('\nRESPONSE\n\n')
-    const response = await fetch(`/api/search?${params}`)
-    // console.log('\nRESPONSE\n\n')
-    // console.log(response)
-    events = await response.json()
-    console.log(events)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function getAllEvents() {
-  try {
-    const response = await fetch('/api/getAllEvents')
-    events = await response.json()
-    // console.log(events)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-createMap(latitude, longitude)
-
-function updateMapZoom(radiusKm) {
-  // console.log('updateMapZoom')
-  // console.log(longitude)
-  // console.log(latitude)
-
-  let zoomLevel
-  if (radiusKm <= 1) {
-    zoomLevel = 13
-  } else if (radiusKm <= 5) {
-    zoomLevel = 12
-  } else if (radiusKm <= 10) {
-    zoomLevel = 11
-  } else if (radiusKm <= 20) {
-    zoomLevel = 10
-  } else {
-    zoomLevel = 9
-  }
-  map.setView([latitude, longitude], zoomLevel)
-}
-
-// Initialize the slider position and value
-updateSlider()

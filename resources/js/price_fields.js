9e8f88c121f8971d805e290bd2962e8efa6782bb
Dranhoc <160327375+Dranhoc@users.noreply.github.com>
2024-07-18 08:59:46 +0200

    [ADD] Price field Sam 2/3

---
+++ resources/js/price_fields.js
@@ -0,0 +1,17 @@
+function toggleFreeCategoriyPrices(element) {
+  const priceSection = element.closest('.price-section')
+  const freeCategoryCheckbox = priceSection.querySelector(".free-category");
+  const regPriceFld = priceSection.querySelector(".regular_price");
+  const discPriceFld = priceSection.querySelector(".discounted_price");
+  const becomeFreeFld = priceSection.querySelector(".become_free");
+
+  if (freeCategoryCheckbox.checked) {
+      regPriceFld.setAttribute("disabled", true)
+      discPriceFld.setAttribute("disabled", true)
+      becomeFreeFld.setAttribute("disabled", true)
+  } else {
+      regPriceFld.removeAttribute("disabled")
+      discPriceFld.removeAttribute("disabled")
+      becomeFreeFld.removeAttribute("disabled")
+  }
+}

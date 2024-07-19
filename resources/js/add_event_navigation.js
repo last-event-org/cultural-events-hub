a07ccc7cac6ccf77536941665a6b9d3ea002e827
samuel-villa <ciullasamuel@gmail.com>
2024-07-19 12:53:47 +0200

    [ADD] frontedn validation when creating event

--- resources/js/add_event_navigation.js
+++ resources/js/add_event_navigation.js
@@ -3,17 +3,67 @@ const steps = document.querySelectorAll('.step');
 const progressBar = document.querySelector('.progress');
 let totalSteps = 7;
 
+function checkSteps(currentStep) {
+  if (currentStep == 0) {
+    let step1ok = true
+    document.getElementById("step1").querySelectorAll("[required]").forEach(function(i) {
+      if (!i.value) {
+        step1ok = false
+      }
+    })
+    if (!step1ok) {
+      alert('Fill all the fields');
+      return false
+    }
+  } else if (currentStep == 1) {
+    let step2ok = true
+    document.getElementById("step2").querySelectorAll("[required]").forEach(function(i) {
+      if (!i.value) {
+        step2ok = false
+      }
+    })
+    if (!step2ok) {
+      alert('Fill all the fields');
+      return false
+    }
+  } else if (currentStep == 3) {
+    let step4ok = false
+    document.getElementsByName("categoryTypes[]").forEach(function(i) {
+      if (i.checked) {
+        step4ok = true
+      }
+    })
+    if (!step4ok) {
+      alert('One category and one subcategory must be filled');
+      return false
+    }
+  } else if (currentStep == 5) {
+    const previewContainer = document.getElementById('imagePreviewContainer')
+    if (previewContainer.childNodes.length === 0) {
+      alert('At least one picture must be added');
+      return false
+    }
+  }
+  return true
+}
+
 function navigateStep(direction) {
   const newStep = currentStep + direction;
 
-  if (newStep >= 0 && newStep < totalSteps) {
-    steps[currentStep].classList.remove('active');
-    steps[newStep].classList.add('active');
-    currentStep = newStep;
-    updateProgressBar();
-    updateStepDisplay();
+  const canContinue = checkSteps(currentStep)
+
+  if (canContinue) {
+    if (newStep >= 0 && newStep < totalSteps) {
+      steps[currentStep].classList.remove('active');
+      steps[newStep].classList.add('active');
+      currentStep = newStep;
+      updateProgressBar();
+      updateStepDisplay();
+    }
+    updateButtons();
+  } else {
+    return
   }
-  updateButtons();
   
 }
 function updateStepDisplay() {

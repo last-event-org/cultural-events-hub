17edfee988c2a00a10b7cb4bdf058a31d6c78c83
samuel-villa <ciullasamuel@gmail.com>
2024-08-01 09:31:13 +0200

    [FIX] zip code validator when creating event

--- app/validators/address.ts
+++ app/validators/address.ts
@@ -5,7 +5,7 @@ export const createAddressValidator = vine.compile(
     name: vine.string().escape().maxLength(255).optional(),
     street: vine.string().escape().maxLength(255),
     number: vine.string().escape().maxLength(10),
-    zip_code: vine.number().positive().withoutDecimals(),
+    zip_code: vine.number().positive().min(1000).max(9999).withoutDecimals(),
     city: vine.string().escape().maxLength(255),
     country: vine.string().escape().maxLength(255),
   })
